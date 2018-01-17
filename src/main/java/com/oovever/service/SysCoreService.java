45b4885ad7159ea4bbc4d1b33ec6d4d184f7b349
Codemupf <mupengfei6688@163.com>
2018-01-17 23:23:55 +0800

    权限缓存实现

--- src/main/java/com/oovever/service/SysCoreService.java
+++ src/main/java/com/oovever/service/SysCoreService.java
@@ -1,17 +1,22 @@
 package com.oovever.service;
 
 import com.google.common.collect.Lists;
+import com.oovever.beans.CacheKeyConstants;
 import com.oovever.common.RequestHolder;
 import com.oovever.dao.SysAclMapper;
 import com.oovever.dao.SysRoleAclMapper;
 import com.oovever.dao.SysRoleUserMapper;
 import com.oovever.model.SysAcl;
 import com.oovever.model.SysUser;
+import com.oovever.util.JsonMapper;
 import org.apache.commons.collections.CollectionUtils;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.stereotype.Service;
-
+import org.codehaus.jackson.type.TypeReference;
 import javax.annotation.Resource;
 import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 /**
  * @author OovEver
@@ -26,7 +31,8 @@ public class SysCoreService {
     private SysRoleUserMapper sysRoleUserMapper;
     @Resource
     private SysRoleAclMapper  sysRoleAclMapper;
-
+    @Resource
+    private SysCacheService sysCacheService;
     /**
      *
      * @return 当前用户包含的所有权限列表
@@ -85,4 +91,47 @@ public class SysCoreService {
         }
         return false;
     }
+    public boolean hasUrlAcl(String url) {
+        if (isSuperAdmin()) {
+            return true;
+        }
+        List<SysAcl> aclList = sysAclMapper.getByUrl(url);
+        if (CollectionUtils.isEmpty(aclList)) {
+            return true;
+        }
+
+        List<SysAcl> userAclList  = getCurrentUserAclListFromCache();
+        Set<Integer> userAclIdSet = userAclList.stream().map(acl -> acl.getId()).collect(Collectors.toSet());
+
+        boolean hasValidAcl = false;
+        // 规则：只要有一个权限点有权限，那么我们就认为有访问权限
+        for (SysAcl acl : aclList) {
+            // 判断一个用户是否具有某个权限点的访问权限
+            if (acl == null || acl.getStatus() != 1) { // 权限点无效
+                continue;
+            }
+            hasValidAcl = true;
+            if (userAclIdSet.contains(acl.getId())) {
+                return true;
+            }
+        }
+        if (!hasValidAcl) {
+            return true;
+        }
+        return false;
+    }
+    public List<SysAcl> getCurrentUserAclListFromCache() {
+        int userId = RequestHolder.getCurrentUser().getId();
+        String cacheValue = sysCacheService.getFromCache(CacheKeyConstants.USER_ACLS, String.valueOf(userId));
+        if (StringUtils.isBlank(cacheValue)) {
+            List<SysAcl> aclList = getCurrentUserAclList();
+            if (CollectionUtils.isNotEmpty(aclList)) {
+                sysCacheService.saveCache(JsonMapper.obj2String(aclList), 600, CacheKeyConstants.USER_ACLS, String.valueOf(userId));
+            }
+            return aclList;
+        }
+        return JsonMapper.string2Obj(cacheValue, new TypeReference<List<SysAcl>>() {
+        });
+    }
+
 }
